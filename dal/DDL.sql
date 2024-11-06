c0d7d0c679108970940be11d6eddf20c69a0a483
𝑰𝒏̃𝒂𝒌𝒊 𝒁𝒂́𝒓𝒂𝒕𝒆 愛 <zaarate7ignacio@gmail.com>
2024-11-05 22:45:51 -0300

    sql nuevo

--- dal/DDL.sql
+++ dal/DDL.sql
@@ -11,16 +11,16 @@ CREATE  TABLE Empleados (
 CREATE TABLE Departamentos (
     id_departamento INT PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR(100) NOT NULL,
-    id_gerente INT,
-    FOREIGN KEY (id_gerente) REFERENCES Empleados(id)
+    id_empleado INT,
+    FOREIGN KEY (id_empleado) REFERENCES Empleados(id_empleado)
 );
 
 CREATE TABLE Empleados_Departamentos (
     id_empleado INT,
     id_departamento INT,
     PRIMARY KEY (id_empleado, id_departamento),
-    FOREIGN KEY (id_empleado) REFERENCES Empleados(id),
-    FOREIGN KEY (id_departamento) REFERENCES Departamentos(id)
+    FOREIGN KEY (id_empleado) REFERENCES Empleados(id_empleado),
+    FOREIGN KEY (id_departamento) REFERENCES Departamentos(id_departamento)
 );
 
 CREATE TABLE Proyectos (
@@ -34,8 +34,8 @@ CREATE TABLE Empleados_Proyectos (
     id_empleado INT,
     id_proyecto INT,
     PRIMARY KEY (id_empleado, id_proyecto),
-    FOREIGN KEY (id_empleado) REFERENCES Empleados(id),
-    FOREIGN KEY (id_proyecto) REFERENCES Proyectos(id)
+    FOREIGN KEY (id_empleado) REFERENCES Empleados(id_empleado),
+    FOREIGN KEY (id_proyecto) REFERENCES Proyectos(id_proyecto)
 );
 
 CREATE TABLE Registro_Tiempo (
@@ -43,10 +43,10 @@ CREATE TABLE Registro_Tiempo (
     id_empleado INT,
     id_proyecto INT,
     fecha VARCHAR(20),
-    horas_trabajadas INT,
+    horas_trabajadas VARCHAR(100),
     descripcion VARCHAR(255),
-    FOREIGN KEY (id_empleado) REFERENCES Empleados(id),
-    FOREIGN KEY (id_proyecto) REFERENCES Proyectos(id)
+    FOREIGN KEY (id_empleado) REFERENCES Empleados(id_empleado),
+    FOREIGN KEY (id_proyecto) REFERENCES Proyectos(id_proyecto)
 );
 
 
