084e384a79b527307cd16fac061cc716753ed794
torresignacio6 <torressnacho54@gmail.com>
2024-11-05 23:35:46 -0300

    Lo hice todo

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ def menu(con):
         print("1. Menú de Empleados")
         print("2. Salir")
         
-        opcion = input("Laburá loco:  ")
+        opcion = input("Ingrese su opcion  ")
         
         if opcion == "1":
             pn.submenu(con)
         elif opcion == "2":
             break
         else:
-            print("Como tan weon.")
+            print("No se encontro")
 
 def main():
     cnx = connection.MySQLConnection(
         user='root', 
         password='', 
         host='127.0.0.1', 
-        database='nachodb',
+        database='gestion_empleados',
         port = 3306
         )
     cursor = cnx.cursor()
